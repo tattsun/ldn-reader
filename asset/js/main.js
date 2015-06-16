38a7772446230258b9525d14d6861f92323fe6d0
tattsun <t.t.mc1192.sf@gmail.com>
2015-06-16 22:36:08 +0900

    fix

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,3 +1,14 @@
+var isMobile = function() {
+    var ua = navigator.userAgent;
+    if(ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0
+       && ua.indexOf('Mobile') > 0) {
+        return true;
+    } else if (ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
+        return true;
+    }
+    return false;
+};
+
 $('ul.slimmenu').slimmenu({
     resizeWidth: '800',
     collapserTitle: 'Livedoor News Reader',
@@ -6,3 +17,9 @@ $('ul.slimmenu').slimmenu({
     indentChildren: false,
     childenIndenter: '&nbsp;'
 });
+
+(function() {
+    if (isMobile() && $('#toMobile').length > 0) {
+        $('#toMobile').html("<a href='/m/top'>モバイルサイトへ</a>");
+    }
+})();
