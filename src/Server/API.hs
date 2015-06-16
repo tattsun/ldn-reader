8ac78b7c23f9f26cb1d1d5a5842d29030083947b
tattsun <t.t.mc1192.sf@gmail.com>
2015-06-16 22:23:46 +0900

    improve ui response

--- src/Server/API.hs
+++ src/Server/API.hs
@@ -2,10 +2,13 @@
 {-# LANGUAGE TemplateHaskell   #-}
 module Server.API where
 
+import           Data.Default
+import           Network.Wai.Middleware.Gzip
 import           Network.Wai.Middleware.Static
 import           Text.Blaze                    (preEscapedText)
 import           Text.Blaze.Html.Renderer.Text (renderHtml)
 import           Text.Hamlet                   (hamletFile)
+import           Text.Julius                   (juliusFile, renderJavascript)
 import           Web.Scotty.Trans
 --
 import           Server.Base
@@ -17,21 +20,22 @@ import qualified Data.Text                     as T
 api :: App ()
 api = do
   middleware $ staticPolicy $ addBase "asset"
-  get "/:tag" $ do
+  middleware $ gzip $ def { gzipFiles = GzipCompress }
+  get "/m/:tag" $ mainView True
+  get "/:tag" $ mainView False
+
+mainView :: Bool -> Act ()
+mainView isMobile = do
     tag <- param "tag"
     rss <- lift . getRSS $ defString2Nt tag
 
     offset <- (read :: String -> Int) <$> paramDefault "offset" "0"
     let articles = drop offset $ unArticleMap rss
---    forM_ articles $ \a -> do
---      lift $ logDebg $ T.concat [ "TITLE: ", asTitle a, "\n"
---                                , "LINK: ", asLink a, "\n"
---                                , "DESC: ", asDescription a, "\n"
---                                ]
 
     articleNum <- confArticleNumPerPage . ctxConfig <$> lift context
 
     html $ renderHtml $ $(hamletFile "view/index.hamlet") undefined
+
 
 defString2Nt :: String -> NewsTag
 defString2Nt str = maybe Top id (string2Nt str)
